92aa56601fc404f53107aa86494765dad1f9e951
Magnus H <me@magnushodne.com>
2022-10-04 12:39:46 +0200

    Added server real slow

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
+import org.eclipse.jetty.server.Server;
+
 public class Main {
     public static void main(String[] args) {
-
+        Server server = new Server(8080);
+        try{
+            server.start();
+        } catch(Exception e) {
+            System.out.println("Well shit");
+        }
     }
+
 }
