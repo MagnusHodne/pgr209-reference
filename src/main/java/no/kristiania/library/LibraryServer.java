7e25847c1cd2531f1d3d7a04637c5b0e0439dc4a
Magnus Hodne <magnus@drobak.org>
2022-10-04 13:21:10 +0200

    Created index.html and modified server

--- src/main/java/no/kristiania/library/LibraryServer.java
+++ src/main/java/no/kristiania/library/LibraryServer.java
@@ -1,6 +1,8 @@
 package no.kristiania.library;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.webapp.WebAppContext;
@@ -10,14 +12,13 @@ public class LibraryServer {
     public LibraryServer(int port){
         this.server = new Server(port);
 
-        WebAppContext apiContext = new WebAppContext();
+        var apiContext = new ServletContextHandler();
         apiContext.setContextPath("/api");
         apiContext.addServlet(new ServletHolder(new BooksApiServlet()), "/books");
-        WebAppContext reactContext = new WebAppContext();
+        var reactContext = new WebAppContext();
         reactContext.setContextPath("/");
         reactContext.setBaseResource(Resource.newClassPathResource("/webapp"));
-        server.setHandler(apiContext);
-        server.setHandler(reactContext);
+        server.setHandler(new HandlerList(apiContext, reactContext));
     }
     public static void main(String[] args) throws Exception{
         var server = new LibraryServer(8080);
