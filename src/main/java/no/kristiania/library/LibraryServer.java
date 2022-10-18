eebcf9fe2f65d7f634ac40279a709b5d1f485e00
Magnus Hodne <magnus@drobak.org>
2022-10-18 11:27:38 +0200

    Successfully deployed to Azure, need to generate frontend as part of build

--- src/main/java/no/kristiania/library/LibraryServer.java
+++ src/main/java/no/kristiania/library/LibraryServer.java
@@ -2,6 +2,7 @@ package no.kristiania.library;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.resource.Resource;
@@ -9,22 +10,40 @@ import org.eclipse.jetty.webapp.WebAppContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.IOException;
+
 public class LibraryServer {
     private final Server server;
     private BookStorage bookStorage = new BookStorage();
 
-    private final Logger logger = LoggerFactory.getLogger(LibraryServer.class);
-    public LibraryServer(int port){
+    private static final Logger logger = LoggerFactory.getLogger(LibraryServer.class);
+
+    private static WebAppContext createWebApp() throws IOException {
+        var webContext = new WebAppContext();
+        webContext.setContextPath("/");
+
+        var resources = Resource.newClassPathResource("/webapp");
+        var sourceDirectory = new File(resources.getFile().getAbsoluteFile().toString()
+                .replace('\\', '/')
+                //Copy the frontend code to the target directory
+                .replace("target/classes", "src/main/resources"));
+        if (sourceDirectory.isDirectory()) {
+            webContext.setBaseResource(Resource.newResource(sourceDirectory));
+            webContext.setInitParameter(DefaultServlet.CONTEXT_INIT + "useFileMappedBuffer", "false");
+        } else {
+            webContext.setBaseResource(resources);
+        }
+        return webContext;
+    }
+    public LibraryServer(int port) throws IOException {
 
         this.server = new Server(port);
 
         var apiContext = new ServletContextHandler();
         apiContext.setContextPath("/api");
         apiContext.addServlet(new ServletHolder(new BooksApiServlet(bookStorage)), "/books");
-        var reactContext = new WebAppContext();
-        reactContext.setContextPath("/");
-        reactContext.setBaseResource(Resource.newClassPathResource("/webapp"));
-        server.setHandler(new HandlerList(apiContext, reactContext));
+        server.setHandler(new HandlerList(apiContext, createWebApp()));
     }
     public static void main(String[] args) throws Exception{
         var server = new LibraryServer(8080);
