582596c5f90ccab4353c760afc87f6e211429f81
Magnus Hodne <magnus@drobak.org>
2022-11-01 12:17:21 +0100

    Removed public qualifiers on interface members

--- src/main/java/no/kristiania/library/BookRepository.java
+++ src/main/java/no/kristiania/library/BookRepository.java
@@ -3,7 +3,7 @@ package no.kristiania.library;
 import java.util.List;
 
 public interface BookRepository {
-    public void save(Book book);
-    public Book retrieve(long id);
-    public List<Book> listAll();
+    void save(Book book);
+    Book retrieve(long id);
+    List<Book> listAll();
 }
