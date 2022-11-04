b036538e1f3432b361975130f37468ac3cb445dc
Magnus H <me@magnushodne.com>
2022-11-04 22:46:17 +0100

    Implemented save and retrieve for books

--- src/main/java/no/kristiania/library/database/JdbcBookRepository.java
+++ src/main/java/no/kristiania/library/database/JdbcBookRepository.java
@@ -3,6 +3,9 @@ package no.kristiania.library.database;
 import jakarta.inject.Inject;
 
 import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class JdbcBookRepository implements BookRepository {
@@ -15,7 +18,21 @@ public class JdbcBookRepository implements BookRepository {
     }
     @Override
     public void save(Book book) {
+        try(var connection = dataSource.getConnection()) {
+            var statement = connection.prepareStatement(
+                    "INSERT INTO books (title, author) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS
+            );
+            statement.setString(1, book.getTitle());
+            statement.setString(2, book.getAuthor());
+            statement.executeUpdate();
 
+            try(var generatedKeys = statement.getGeneratedKeys()) {
+                generatedKeys.next();
+                book.setId(generatedKeys.getInt(1));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -25,6 +42,21 @@ public class JdbcBookRepository implements BookRepository {
 
     @Override
     public List<Book> listAll() {
-        return null;
+        try(var connection = dataSource.getConnection()) {
+            var statement = connection.prepareStatement("SELECT * FROM books");
+            try(var rs = statement.executeQuery()) {
+                var list = new ArrayList<Book>();
+                while(rs.next()) {
+                    var book = new Book();
+                    book.setId(rs.getInt("id"));
+                    book.setTitle(rs.getString("title"));
+                    book.setAuthor(rs.getString("author"));
+                    list.add(book);
+                }
+                return list;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
