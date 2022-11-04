2543cea28e5969d93b2debdfb1ab7fd030d86a6a
Magnus H <me@magnushodne.com>
2022-11-04 22:38:01 +0100

    Made id settable

--- src/main/java/no/kristiania/library/database/Book.java
+++ src/main/java/no/kristiania/library/database/Book.java
@@ -1,25 +1,17 @@
 package no.kristiania.library.database;
 
 public class Book {
-    private final int id;
+    private int id;
     private String title;
     private String author;
-
-    public Book(
-            int id,
-            String title,
-            String author
-    ) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-    }
 
     public int getId() {
         return id;
     }
 
-
+    public void setId(int id) {
+        this.id = id;
+    }
     public String getTitle() {
         return title;
     }
