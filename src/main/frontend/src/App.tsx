4e8e6ff6f603ff16a588ed183b163387a788309b
Magnus H <me@magnushodne.com>
2022-11-04 22:58:16 +0100

    Implemented frontend

--- src/main/frontend/src/App.tsx
+++ src/main/frontend/src/App.tsx
@@ -6,7 +6,44 @@ function App() {
 
   return (
     <div className="App">
+      <AddBook setBooks={setBooks} />
       <ListBooks books={books} setBooks={setBooks} />
+    </div>
+  );
+}
+
+function AddBook({ setBooks }: { setBooks: any }) {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  async function handleSubmit(e: any) {
+    e.preventDefault();
+    setLoading(true);
+    await fetch("/api/books", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title, author }),
+    });
+    window.location.reload();
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h3>Add book</h3>
+        <div>
+          <label>Author</label>
+          <input type="text" onChange={(e) => setAuthor(e.target.value)} />
+        </div>
+        <div>
+          <label>Title</label>
+          <input type="text" onChange={(e) => setTitle(e.target.value)} />
+        </div>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
@@ -20,14 +57,20 @@ function ListBooks({ books, setBooks }: { books: any[]; setBooks: any }) {
     setBooks(books);
     setLoading(false);
   }, []);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
   return (
-    <ul>
-      {books.map((book, index) => (
-        <li key={index}>
-          Book: {book.title}, Author: {book.author}
-        </li>
-      ))}
-    </ul>
+    <div>
+      <h3>Books</h3>
+      <ul>
+        {books.map((book, index) => (
+          <li key={index}>
+            Book: {book.title}, Author: {book.author}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
