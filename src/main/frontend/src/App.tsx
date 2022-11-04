b326a24cd0080e1934344addb40d93e0babf44e5
Magnus H <me@magnushodne.com>
2022-11-04 22:35:30 +0100

    Started coding actual frontend

--- src/main/frontend/src/App.tsx
+++ src/main/frontend/src/App.tsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [books, setBooks] = useState([]);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ListBooks books={books} setBooks={setBooks} />
     </div>
-  )
+  );
 }
 
-export default App
+function ListBooks({ books, setBooks }: { books: any[]; setBooks: any }) {
+  const [loading, setLoading] = useState(true);
+  // @ts-ignore
+  useEffect(async () => {
+    const response = await fetch("/api/books");
+    const books = await response.json();
+    setBooks(books);
+    setLoading(false);
+  }, []);
+  return (
+    <ul>
+      {books.map((book, index) => (
+        <li key={index}>
+          Book: {book.title}, Author: {book.author}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default App;
